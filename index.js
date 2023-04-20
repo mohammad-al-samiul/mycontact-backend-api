c58e1c789116b5021b2c1f27bf470bd710f0ccd9
Noob-H4cker <alsamiul123@gmail.com>
2023-04-20 14:43:51 +0600

    successfully database connetion

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('./config/dbConnection');
 
 const dotenv = require('dotenv').config();
 
 const Port = process.env.PORT || 5001;
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use('/api/contact',require('./routes/contactRoutes'));
